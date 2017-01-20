e26cf9876c75e4e7e8324f12317554063e2943bb
Jason Pilz <jasonpilz@gmail.com>
2017-01-19 20:13:45 -0800

    Add tests for board

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source 'https://rubygems.org'
 
 gem 'pry'
 gem 'rubocop'
+gem 'minitest'
+gem 'rake'
---
+++ test/sudoku/board_test.rb
@@ -0,0 +1,46 @@
+require 'test_helper'
+
+class Sudoku::BoardTest < Minitest::Test
+  def setup
+    @invalid_board = [
+      [1, 2, 3, 4, 5, 6, 7, 8, 9],
+      [1, 2, 3, 4, 5, 6, 7, 8, 9],
+      [1, 2, 3, 4, 5, 6, 7, 8, 9],
+      [1, 2, 3, 4, 5, 6, 7, 8, 9],
+      [1, 2, 3, 4, 5, 6, 7, 8, 9],
+      [1, 2, 3, 4, 5, 6, 7, 8, 9],
+      [1, 2, 3, 4, 5, 6, 7, 8, 9],
+      [1, 2, 3, 4, 5, 6, 7, 8, 9],
+      [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    ]
+
+    @valid_board = [
+      [5, 3, 4, 6, 7, 8, 9, 1, 2],
+      [6, 7, 2, 1, 9, 5, 3, 4, 8],
+      [1, 9, 8, 3, 4, 2, 5, 6, 7],
+      [8, 5, 9, 7, 6, 1, 4, 2, 3],
+      [4, 2, 6, 8, 5, 3, 7, 9, 1],
+      [7, 1, 3, 9, 2, 4, 8, 5, 6],
+      [9, 6, 1, 5, 3, 7, 2, 8, 4],
+      [2, 8, 7, 4, 1, 9, 6, 3, 5],
+      [3, 4, 5, 2, 8, 6, 1, 7, 9]
+    ]
+  end
+
+
+  def test_it_returns_invalid_for_invalid_board
+    sudoku_validator = Sudoku::Board.new(@invalid_board)
+
+    result = sudoku_validator.validate
+
+    assert_equal result, 'invalid'
+  end
+
+  def test_it_returns_valid_for_valid_board
+    sudoku_validator = Sudoku::Board.new(@valid_board)
+
+    result = sudoku_validator.validate
+
+    assert_equal result, 'valid'
+  end
+end
